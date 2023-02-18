73bc5f104a9e0a6b90563d0ffcf066721c8c1819
960ptou <374780732weijie@gmail.com>
2023-02-18 13:27:21 -0500

    db design

--- setup.sql
+++ setup.sql
@@ -1,5 +1,21 @@
 CREATE TABLE users (
     id TEXT PRIMARY KEY,
     username TEXT,
-    password TEXT
+    password TEXT,
 );
+
+-- Optional feature, may or may not use
+CREATE TABLE user_stat(
+    id TEXT UNIQUE,
+    wins INTEGER,
+    total INTEGER,
+    FOREIGN KEY(id) REFERENCES users(id),
+)
+
+CREATE TABLE user_deck( -- We can limit on JS side
+    deckid TEXT PRIMARY KEY, -- UUID ?
+    userid TEXT,
+    deckname TEXT,
+    deck TEXT, -- Should be JSON string list : ex : [1,2,3,4,...]
+    FOREIGN KEY(userid) REFERENCES users(id)
+)
