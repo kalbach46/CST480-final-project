40658d96d7a3b36ebf6b37c4a562feac9da02b53
960ptou <374780732weijie@gmail.com>
2023-02-22 09:14:23 -0500

    fix

--- setup.sql
+++ setup.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id TEXT PRIMARY KEY,
     username TEXT,
-    password TEXT,
+    password TEXT
 );
 
 -- Optional feature, may or may not use
@@ -9,8 +9,8 @@ CREATE TABLE user_stat(
     id TEXT UNIQUE,
     wins INTEGER,
     total INTEGER,
-    FOREIGN KEY(id) REFERENCES users(id),
-)
+    FOREIGN KEY(id) REFERENCES users(id)
+);
 
 CREATE TABLE user_deck( -- We can limit on JS side
     deckid TEXT PRIMARY KEY, -- UUID ?
@@ -18,4 +18,4 @@ CREATE TABLE user_deck( -- We can limit on JS side
     deckname TEXT,
     deck TEXT, -- Should be JSON string list : ex : [1,2,3,4,...]
     FOREIGN KEY(userid) REFERENCES users(id)
-)
+);
